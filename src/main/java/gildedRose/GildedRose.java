4d7a1d6ba3b47c789d76e7be2305eb26e1bf3d9d
FelicityChen <felicity.chen@oocl.com>
2019-08-12 23:02:00 +0800

    refactor: remove redundant code by extract sellIn compare business

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -12,7 +12,7 @@ public class GildedRose {
         for (; i < items.length; i++) {
             if (!isSpecifiedItem("Aged Brie")
                     && !isSpecifiedItem("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
+                if (isQualityFloorValid()) {
                     if (!isSpecifiedItem("Sulfuras, Hand of Ragnaros")) {
                         if (isSpecifiedItem("Conjured Good")) {
                             decrese1Quality();
@@ -21,18 +21,18 @@ public class GildedRose {
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (isQualityCeilValid()) {
                     increase1Quality();
 
                     if (isSpecifiedItem("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        if (isBackstagePassesSellInLessThan11()) {
+                            if (isQualityCeilValid()) {
                                 increase1Quality();
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (isBackstagePassesSellInLessThanSix()) {
+                            if (isQualityCeilValid()) {
                                 increase1Quality();
                             }
                         }
@@ -41,13 +41,13 @@ public class GildedRose {
             }
 
             if (!isSpecifiedItem("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+                Decrese1SellIn();
             }
 
             if (items[i].sellIn < 0) {
                 if (!isSpecifiedItem("Aged Brie")) {
                     if (!isSpecifiedItem("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
+                        if (isQualityFloorValid()) {
                             if (!isSpecifiedItem("Sulfuras, Hand of Ragnaros")) {
                                 if (isSpecifiedItem("Conjured Good")) {
                                     decrese1Quality();
@@ -59,12 +59,32 @@ public class GildedRose {
                         setQuality0();
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (isQualityCeilValid()) {
                         increase1Quality();
                     }
                 }
             }
         }
+    }
+
+    private boolean isBackstagePassesSellInLessThanSix() {
+        return items[i].sellIn < 6;
+    }
+
+    private boolean isBackstagePassesSellInLessThan11() {
+        return items[i].sellIn < 11;
+    }
+
+    private void Decrese1SellIn() {
+        items[i].sellIn = items[i].sellIn - 1;
+    }
+
+    private boolean isQualityCeilValid() {
+        return items[i].quality < 50;
+    }
+
+    private boolean isQualityFloorValid() {
+        return items[i].quality > 0;
     }
 
     private void setQuality0() {
