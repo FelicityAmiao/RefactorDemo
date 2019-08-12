6215ea3d5b2900659ab05f9c09af409eb1b629d6
FelicityChen <felicity.chen@oocl.com>
2019-08-12 21:54:18 +0800

    test: test sellin 0 normal good will degrade double quality

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -15,4 +15,14 @@ public class GildedRoseTest {
 
         assertEquals("Normal Good, 3, 2", normalGood.toString());
     }
+
+    @Test
+    public void should_return_itemWithDoubleDegradesQualityMinus1SellIn_when_call_updateQuality_given_0SellIn3QualityNormalGood() {
+        Item normalGood = new Item("Normal Good", 0, 3);
+        Item[] items = {normalGood};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+
+        assertEquals("Normal Good, -1, 1", normalGood.toString());
+    }
 }
